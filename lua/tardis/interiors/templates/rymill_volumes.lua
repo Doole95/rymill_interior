169e0ccdbd992f8da97b275716ec06f685fffb64
Dayle Smith <dallius95@gmail.com>
2024-02-20 19:55:06 +0000

    added volume options, blue roundles options

---
+++ lua/tardis/interiors/templates/rymill_volumes.lua
@@ -0,0 +1,20 @@
+TARDIS:AddInteriorTemplate("rymill_volon", {
+})
+
+TARDIS:AddInteriorTemplate("rymill_voloff", {
+	Interior = {
+
+		Parts={
+			rymill_vol_light = false,
+			rymill_vol_lightdownlight1 = false,		
+			rymill_vol_lightdownlight2 = false,		
+			rymill_vol_lightdownlight3 = false,		
+			rymill_vol_lightdownlight4 = false,		
+			rymill_vol_lightdownlight5 = false,
+		},
+	},
+
+
+    
+})
+
